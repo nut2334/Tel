f70b0a7fa5e0470003892cf73ac27c8919898459
Natcharika <s6304062620061@email.kmutnb.ac.th>
2022-10-08 23:29:15 +0700

    sleep

---
+++ RegisterNewCustomer.php
@@ -0,0 +1,20 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=telephone;charset=utf8", "root", "");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); ?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ลงทะเบียนข้อมูลลูกค้า</title>
+    </head>
+    <body>
+        ชื่อ-สกุล:
+        <select name="cus_prefix">
+            <option value='นาย'>นาย</option>
+            <option value='นาง'>นาง</option>
+            <option value='นางสาว'>นางสาว</option>
+        </select>
+        <input type='text' name="cus_name"><br>
+        เบอร์โทรศัพท์: <input type='tel' name="cus_tel"><br>
+        <input type='submit' value = 'ลงทะเบียน'>
+    </body>
+</html>
