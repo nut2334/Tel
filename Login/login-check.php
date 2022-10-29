f50f00731c200022e35178f9435f29409d9a1514
nut2334 <92030101+nut2334@users.noreply.github.com>
2022-10-29 20:09:34 +0700

    hi

--- Login/login-check.php
+++ Login/login-check.php
@@ -14,7 +14,21 @@ if(!empty($row)){
     $_SESSION["username"]=$row["emp_tel"];
 
     echo "เข้าสู่ระบบสำเร็จ<br>";
-    echo "<a href='search.php'>ไปยังหน้าหลัก</a><br>";
+    $value=$_POST["employee_id"];
+    $check="select * from repairman where employee_id = '$value'";
+    $stmt2 = $pdo->prepare($check);
+    $stmt2->execute();
+    $row2 = $stmt2->fetch();
+    //$_SESSION["username"]=$row["emp_tel"];
+    
+    //$result=mysql_query($check) or die(mysql_error());
+    //$num=mysql_num_rows($result);
+    if(empty($row2)){
+        echo "<a href='search.php'>ไปยังหน้าหลัก</a><br>";
+    }
+    else{
+        echo "<a href='search_repairman.php'>ไปยังหน้าหลัก</a><br>";
+    }
 }
 else{
     echo "ไม่สำเร็จ ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง";
